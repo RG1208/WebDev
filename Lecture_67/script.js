215a3f34317a44c9eba5349edd7b6cc863147b1b
rachit <rachitgarg1208@gmail.com>
2024-05-20 19:04:44 +0530

    DOM Tree

---
+++ Lecture_67/script.js
@@ -0,0 +1,21 @@
+document.body.childNodes
+document.body.children
+
+document.body.childNodes[1]
+document.body.childNodes[1].childNodes
+let cont = document.body.childNodes
+cont
+cont.firstChild
+
+cont.firstElementChild
+cont.lastElementChild.style.backgroundColor = "red"
+
+// children gives only the html elements
+cont.children
+
+cont.children[2]
+cont.children[2].nextElementSibling
+cont.children[2].previousElementSibling
+
+
+
