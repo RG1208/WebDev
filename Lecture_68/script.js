c98ac517182a0d2b32e38ac82ebdc1aefd0bc58a
rachit <rachitgarg1208@gmail.com>
2024-05-21 22:54:34 +0530

    DOM

---
+++ Lecture_68/script.js
@@ -0,0 +1,17 @@
+document.body.getElementsByClassName("box")
+
+let cont = document.body.getElementsByClassName("box")
+cont[2].style.backgroundColor = "red"
+
+document.getElementById("box5")
+let idbox = document.getElementById("box5")
+idbox.style.backgroundColor = "Blue"
+
+document.querySelector(".box").style.backgroundColor = "aqua";
+
+// document.querySelectorAll(".box").forEach(e => {
+//     e.style.backgroundColor = "brown";
+
+// });
+
+console.log(document.getElementsByTagName("div"))
