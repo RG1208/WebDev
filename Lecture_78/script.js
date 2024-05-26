930b11e58babb425009af4d095e80b3d4ef4165d
rachit <rachitgarg1208@gmail.com>
2024-05-26 17:22:25 +0530

    Exercise_14

---
+++ Lecture_78/script.js
@@ -0,0 +1,29 @@
+const addItem = async (item) => {
+    await randomDelay();
+    let div = document.createElement("div");
+    div.innerHTML = item;
+    document.body.append(div)
+}
+
+const randomDelay = () => {
+    return new Promise((resolve, reject) => {
+        timeout = 1 + 6 * Math.random()
+        setTimeout(() => {
+            resolve()
+        }, timeout * 1000);
+
+    })
+
+}
+
+let text = [
+    "Intializing Hacking ...",
+    "Reading your Files ...",
+    "Sending all Passwords and Files to cloud ...",
+    "Cleaning up your PC ..."
+]
+
+for (const item of text) {
+    addItem(item)
+}
+
